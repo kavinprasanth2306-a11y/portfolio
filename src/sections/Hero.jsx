9232d3022860e904fca771d655131f22355f4307
kavinprasanth km <kavinprasanth2306@gmail.com>
2026-09-09 18:35:02 +0530

    Update Hero.jsx

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import { TypeAnimation } from 'react-type-animation'
 import { FaGithub, FaLinkedinIn } from 'react-icons/fa'
-import { HiDownload } from 'react-icons/hi'
+import { HiEye } from 'react-icons/hi'
 import { motion } from 'framer-motion'
 
 export default function Hero() {
